1c743e6ddaa8dd1f34c7395a2e490c2df0b15bab
allencwwong <allencwwong@gmail.com>
2016-05-01 21:39:23 -0700

    index page and detail page

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,42 +1,43 @@
 var ready;
 
 ready = function(){
-  
+
   // filter btn
 
   $(".filter-btn").click(function(){
     $("#filter_panel").slideToggle("slow");
   });
-    
+
     // show page image selector
-      $(".sub-image li img").click(function(){
+    $(".sub-image li img").click(function(){
       var selected_img = $(this).attr("src");
       $(".main-image").attr("src",selected_img);
       
   // show page name
-      var name = $(this).data("name");
-      $(".header-title h2").append().text(name);
-      console.log(name);
+  var name = $(this).data("name");
+  $(".header-title h2").append().text(name);
+  console.log(name);
 
-    });  
+});  
 
   // show sub navigation
 
-    $("div").each(function(){
-      if($(this).data("nav") === "sub-nav"){
-        $("#main_nav").css("display","none");
-        $("#sub_nav").css("display","block");
-      }
-    }); 
+  $("div").each(function(){
+    if($(this).data("nav") === "sub-nav"){
+      $("#main_nav").css("display","none");
+      $("#sub_nav").css("display","block");
+    }
+  }); 
 
   // sub navigation back/home button
-    
-    $(".back-btn").click(function(){
-    
-  
-        window.location.replace("http://localhost:3001");
-    
-    });  
+
+  $(".back-btn").click(function(){   
+    parent.history.back();
+    return false;    
+  });  
+
+  // food tags
+
 
 
 };
@@ -44,8 +45,8 @@ ready = function(){
 $(document).ready(ready);
 $(document).on('page:load', ready);
 
-    
-    
+
+
 
 
 
--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -11,16 +11,44 @@ class FoodsController < ApplicationController
   end
 
   def show
+
+    #create food tags 
+
+    @food_tags = FoodTag.all
+    @food_tag_list = []
+    food_tag_ids = []
+
+    @food_tags.each do |tag|
+      food_tag_ids.push(tag.food_photo_id)
+    end
+
+    food_tag_ids.uniq!
+
+      food_tag_ids.each do |id|
+        food_tag = Hash.new
+          food_tag[:id] = id
+          index = 0
+          @food_tags.each do |tag|
+            if tag.food_photo_id == id
+              food_tag[index] = tag.tag_name
+              index+=1
+            end
+          end
+        @food_tag_list.push(food_tag)
+    end
+
+    @current_restaurant = Store.find(params[:id])
     main_photo_id = params[:photo].to_i
     main_photo_id_index = main_photo_id-1
-    @main_photo_url = Store.find(params[:id]).food_photos.find(main_photo_id).photo_url
-    @main_photo_name = Store.find(params[:id]).food_photos.find(main_photo_id).food_name
-    restaurant_photo_collection = Store.find(params[:id]).food_photos
+    @main_photo_url = @current_restaurant.food_photos.find(main_photo_id).photo_url
+    @main_photo_name = @current_restaurant.food_photos.find(main_photo_id).food_name
+    restaurant_photo_collection = @current_restaurant.food_photos
     @restaurant_photos = []
     restaurant_photo_collection.each do |rpc|
       @restaurant_photos << rpc
     end
     @restaurant_photos_ordered = @restaurant_photos.reverse!
     @restaurant_photos_ordered.insert(0,@restaurant_photos.delete_at(main_photo_id_index))
+
   end
 end
